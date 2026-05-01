91e5dde7f94a959d1010b92a9e6b77fd9cfa372c
c3n9 <108518693+c3n9@users.noreply.github.com>
2026-05-01 23:09:35 +0300

    Update table.js

--- webview/table.js
+++ webview/table.js
@@ -213,26 +213,6 @@ document.addEventListener('keydown', (e) => {
 
 
 // search
-function search() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("searchInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("json-table");
-  tr = table.getElementsByTagName("tr");
-  // Перебирайте все строки таблицы и скрывайте тех, кто не соответствует поисковому запросу
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
 
 function clearHighlights() {
   document.querySelectorAll('#json-table td').forEach(td => {
